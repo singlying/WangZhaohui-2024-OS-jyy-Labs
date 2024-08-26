9a2eb36dae5ce7376258513258459b1a57c9602d
tracer-nju <tracer@nju.edu.cn>
2024-08-26 21:54:18 +0800

    Linux SINGLYING-LAPTOP 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:54:18 up  3:27,  1 user,  load average: 0.08, 0.15, 0.11

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -20,6 +20,7 @@ void print_key() {
   AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
   ioe_read(AM_INPUT_KEYBRD, &event);
   if (event.keycode != AM_KEY_NONE && event.keydown) {
+    printf("Keycode: %d\n", event.keycode);
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
