10dc5f9142fdd1d4297d2afab548002eacab37bd
tracer-nju <tracer@nju.edu.cn>
2024-08-26 22:13:20 +0800

    Linux SINGLYING-LAPTOP 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:13:20 up  3:46,  1 user,  load average: 0.44, 0.24, 0.13

--- .shadow/hello/hello.c
+++ .shadow/hello/hello.c
@@ -72,19 +72,6 @@ void draw_image(uint32_t *image_data, int img_width, int img_height) {
   }
 }
 
-// 键盘事件处理
-// void print_key()
-// {
-//   AM_INPUT_KEYBRD_T event = {.keycode = AM_KEY_NONE};
-//   ioe_read(AM_INPUT_KEYBRD, &event);
-//   if (event.keycode != AM_KEY_NONE && event.keydown)
-//   {
-//     puts("Key pressed: ");
-//     puts(key_names[event.keycode]);
-//     puts("\n");
-//   }
-// }
-
 int main(const char *args)
 {
   // 初始化 I/O 环境
