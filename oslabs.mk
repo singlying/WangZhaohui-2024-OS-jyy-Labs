c9256a6b1374c2ddccf11caa006e070a90624503
WangZhaohui <singlying@outlook.com>
2024-08-28 14:42:20 +0800

    add .mk

---
+++ oslabs.mk
@@ -0,0 +1,11 @@
+export TOKEN := ???
+
+# ----- DO NOT MODIFY -----
+
+export COURSE := OS2024
+URL := 'https://jyywiki.cn/submit.sh'
+
+submit:
+	@cd $(dir $(abspath $(lastword $(MAKEFILE_LIST)))) && \
+	  curl -sSLf '$(URL)' > /dev/null && \
+	  curl -sSLf '$(URL)' | bash
